373967c98da7f411d6ba8e7590d690e5745309ef
Austin Loos <aero.loos@gmail.com>
2020-04-23 23:57:13 -0700

    Began databases in Python

---
+++ Databases/tables.py
@@ -0,0 +1,18 @@
+# Import everything from the peewee library.
+from peewee import *
+
+# Now we need to make a database connection. Make an SqliteDatabase() named "challenges.db". Assign it to the variable db.
+db = SqliteDatabase('challenges.db')
+
+# Alright, now for the biggest part. Make a model named Challenge that has two fields, name and language. Both fields should be of the type CharField with a max_length of 100.
+class Challenge(Model):
+    name = CharField(max_length=100)
+    language = CharField(max_length=100)
+
+    # Now add a Meta class to Challenge and set the database attribute equal to db
+    class Meta():
+        database = db
+
+if __name__ == "__main__":
+    db.connect()
+    db.create_tables(['Name', 'Language'], safe=True)
