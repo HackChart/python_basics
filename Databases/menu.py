5b0d2d2bffb75840e4854b36e63013baa320ed58
Austin Loos <hackchart@gmail.com>
2020-05-02 19:44:53 -0700

    Create menu.py

---
+++ Databases/menu.py
@@ -0,0 +1,11 @@
+# Uses OrderedDict to create an interactive menu
+
+## Menu Items:
+# 'n', 'new challenge'
+# 's', 'new step'
+# 'd', 'delete a challenge'
+# 'e', 'edit a challenge'
+
+from collections import OrderedDict
+
+menu = OrderedDict([('n', 'new challenge'),('s', 'new step'), ('d', 'delete a challenge'), ('e', 'edit a challenge')])
